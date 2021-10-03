4bb38c744d091ab80d13dc3f3994ed4f0759e910
mario <mario@mario-VirtualBox>
2021-10-03 16:43:36 +0200

    prueba de integracion junit

---
+++ src/main/java/grupo/mavenproject/App.java
@@ -0,0 +1,10 @@
+package grupo.mavenproject;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+  public void mostrar() {
+	  System.out.println("Hola mundo");
+  }
+}
---
+++ src/test/java/grupo/mavenproject/testMostrar.java
@@ -0,0 +1,19 @@
+package grupo.mavenproject;
+
+import junit.framework.TestCase;
+
+public class testMostrar extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testMostrar() {
+		fail("Not yet implemented");
+	}
+
+}
